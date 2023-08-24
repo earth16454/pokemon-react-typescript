38231df8b87dff27246449829897bfb98a4b2d9e
Thanaphong.S <thanaphong.s@humanica.com>
2023-08-24 11:59:22 +0700

    refactor: Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,9 +4,40 @@ import type { MenuProps } from "antd";
 import { NavLink } from "react-router-dom";
 
 const { Header } = Layout;
-
-// interface 
-
+const items: MenuProps["items"] = [
+  {
+    label: (
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >Home</NavLink>
+    ),
+    key: "home",
+  },
+  {
+    label: (
+      <NavLink
+        to="/details/1"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >Pokemon Profile</NavLink>
+    ),
+    key: "profile",
+  },
+  {
+    label: "About",
+    key: "about",
+    disabled: true,
+  },
+  {
+    label: "Contact",
+    key: "contact",
+    disabled: true,
+  },
+];
 
 const Navbar = () => {
   const [current, setCurrent] = useState("home");
@@ -19,6 +50,13 @@ const Navbar = () => {
   return (
     <Layout>
       <Header>
+        <Menu
+          theme="dark"
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+        />
       </Header>
     </Layout>
   );
